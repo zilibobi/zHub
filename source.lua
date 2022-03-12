7672aae0a9b605e694b3009156b12ef8d0b52a27
zilibobi <81919798+zilibobi@users.noreply.github.com>
2022-03-12 20:35:57 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -1064,17 +1064,17 @@ addType(cc, "checkbox", "Float", "float", false, function(check)
 
 					cons[1] = UserInputService.InputBegan:Connect(function(input, gp)
 						if not gp and input.KeyCode == Enum.KeyCode.Q then
-							dir -= typeData.floatSpeed / 10
+							dir -= 0.5
 						elseif not gp and input.KeyCode == Enum.KeyCode.E then
-							dir += typeData.floatSpeed / 10
+							dir += 0.5
 						end
 					end)
 
 					cons[2] = UserInputService.InputEnded:Connect(function(input, gp)
 						if not gp and input.KeyCode == Enum.KeyCode.Q then
-							dir += typeData.floatSpeed / 10
+							dir += 0.5
 						elseif not gp and input.KeyCode == Enum.KeyCode.E then
-							dir -= typeData.floatSpeed / 10
+							dir -= 0.5
 						end
 					end)
 
@@ -1109,7 +1109,6 @@ end)
 addType(cc, "checkbox", "CFrame Walk", "cfWalk", false)
 
 addType(cc, "slider", "Fly Speed", "flySpeed", { start = 50, min = 5, max = 300 })
-addType(cc, "slider", "Float Speed", "floatSpeed", { start = 5, min = 5, max = 35 })
 
 addType(cc, "slider", "CFWalk Speed", "cfWalkSpeed", { start = 16, min = 5, max = 300 }, function()
 	RunService:BindToRenderStep("CFWalkSpeed", Enum.RenderPriority.First.Value, function()
